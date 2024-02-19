720483ec8d455292efd8e8ad1e237209b6918577
Stivan Dimitrov <stivandimitrov27@gmail.com>
2024-02-19 22:09:31 +0200

    Added solution

---
+++ 04_JS_front_end/JS Font-End/01_JS_syntax_fundamentals/1.2_JS_syntax_fundamentals_exercise/07_Chars-to-String.js
@@ -0,0 +1,9 @@
+function solve(character1, character2, character3) {
+    let result = character1 + character2 + character3
+    
+    console.log(result);
+}
+
+// solve('a', 'b', 'c');
+// solve('%', '2', 'o');
+// solve('1', '5', 'p');s
