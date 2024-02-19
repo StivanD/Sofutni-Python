e84e3962086da04e88f9da09b3beb77bd8d52e73
Stivan Dimitrov <stivandimitrov27@gmail.com>
2024-02-19 22:12:35 +0200

    Added solution

---
+++ 04_JS_front_end/JS Font-End/01_JS_syntax_fundamentals/1.2_JS_syntax_fundamentals_exercise/08_Reversed-Chars.js
@@ -0,0 +1,8 @@
+function solve(character1, character2, character3) {
+    let result = `${character3} ${character2} ${character1}`;
+    
+    console.log(result);
+}
+
+// solve('A', 'B', 'C');
+// solve('1', 'L', '&');
